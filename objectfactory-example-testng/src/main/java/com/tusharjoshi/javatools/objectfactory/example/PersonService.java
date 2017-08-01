800417d13cfe977084fe5dffca575ab6f2f84f85
Tushar Joshi <tusharvjoshi@gmail.com>
2017-08-01 20:32:02 +0530

    Made Person and PersonService unempty

--- objectfactory-example-testng/src/main/java/com/tusharjoshi/javatools/objectfactory/example/PersonService.java
+++ objectfactory-example-testng/src/main/java/com/tusharjoshi/javatools/objectfactory/example/PersonService.java
@@ -4,12 +4,11 @@ import com.tusharjoshi.javatools.objectfactory.ObjectFactory;
 
 public class PersonService {
 
-	public Person enrollPerson() {
-		Person person = ObjectFactory.build(Person.class)
-				.withTypes(String.class, int.class)
-				.withArgs("John Doe", 29)
-				.create();
-		return person;
-	}
+  public Person enrollPerson() {
+      return ObjectFactory.build(Person.class)
+              .withTypes(String.class, int.class)
+              .withArgs("John Doe", 29)
+              .create();
+  }
 
 }
