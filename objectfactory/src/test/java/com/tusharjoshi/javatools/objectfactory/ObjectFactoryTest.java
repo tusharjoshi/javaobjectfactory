a0ed7e7a93609f621806632459e5f97e101c6d69
Tushar Joshi <tusharvjoshi@gmail.com>
2017-08-01 22:18:37 +0530

    Add more coverage to ObjectFactory

--- objectfactory/src/test/java/com/tusharjoshi/javatools/objectfactory/ObjectFactoryTest.java
+++ objectfactory/src/test/java/com/tusharjoshi/javatools/objectfactory/ObjectFactoryTest.java
@@ -5,27 +5,41 @@ import org.junit.Before;
 import org.junit.Test;
 
 public class ObjectFactoryTest {
-  
+
   private ObjectFactory<String> target;
-  
+
   @Before
   public void setUp() {
     target = ObjectFactory.build(String.class);
   }
-  
-  @Test (expected=ObjectFactoryException.class)
+
+  @Test(expected = ObjectFactoryException.class)
   public void testBuildNull() {
     ObjectFactory.build(null).create();
   }
-  
-  @Test (expected=ObjectFactoryException.class)
+
+  @Test(expected = ObjectFactoryException.class)
   public void testBuildCreateException() {
     ObjectFactory.build(ObjectWithException.class).create();
+  }
+
+  @Test(expected = ObjectFactoryException.class)
+  public void testBuildWithArgsCreateException() {
+    ObjectFactory.build(ObjectWithException.class)
+      .withArgs("Hello")
+      .create();
   }
 
   @Test
   public void testBuild() {
     Assert.assertNotNull(target);
+  }
+
+  @Test
+  public void testCreate() {
+    String value = target.create();
+    Assert.assertNotNull(value);
+    Assert.assertEquals("", value);
   }
 
   @Test
@@ -41,9 +55,17 @@ public class ObjectFactoryTest {
     Assert.assertNotNull(value);
     Assert.assertEquals("Hello", value);
   }
-  
+
   public static class ObjectWithException {
+    @SuppressWarnings("unused")
+    private String param = null;
+
     public ObjectWithException() {
+      throw new ObjectFactoryException("");
+    }
+
+    public ObjectWithException(String param) {
+      this.param = param;
       throw new ObjectFactoryException("");
     }
   }
